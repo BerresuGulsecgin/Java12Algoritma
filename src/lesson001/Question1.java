c89e12978905d5161211a690c0bd62f8608781b3
unknown <gulsecginberresu@gmail.com>
2023-08-14 14:58:23 +0300

    Question2 ve Question3 cozum

--- src/lesson001/Question1.java
+++ src/lesson001/Question1.java
@@ -4,7 +4,11 @@ public class Question1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Github denemesi")
+		System.out.println("Github denemesi");
+		
+		System.out.println("sss");
+		
+		// Clas isimleri PascalCase tarzında isimlendirilirkelime başları büyük
 
 	}
 
